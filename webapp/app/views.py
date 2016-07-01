fb4481b850efa23721d78708c1c14c9f93c916d3
monburan <mengbulang012@gmail.com>
2016-07-01 21:23:26 +0800

    修改：     webapp/app/templates/index.html 	修改：     webapp/app/views.py 	修改：     webapp/app/views.pyc

--- webapp/app/views.py
+++ webapp/app/views.py
@@ -5,5 +5,23 @@ from flask import render_template
 @app.route('/index')
 def index():
     user = {'nickname':'monburan'}
-    return render_template("index.html",title = "Home",user = user)
+    posts = [
+            {
+                'author':{'nickname':'mazaiku'},
+                'body':'fake user 1'
+                },
+            {
+                'author':{'nickname':'lake2'},
+                'body':'fake user 2'
+                },
+            {
+                'author':{'nickname':'evilcos'},
+                'body':'fake user 3'
+                }
+            ]
+    return render_template(
+            "index.html",
+            title = "Home",
+            user = user,
+            posts = posts)
     #return "Hello World! flask"
